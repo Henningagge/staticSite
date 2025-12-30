e11a59e9c273a447b5222ab81fa1c6c2ac88299d
heagge <henning.agge@gmail.com>
2025-12-30 16:29:33 +0100

    leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,8 +11,23 @@ class HTMLNode():
         if self.props == {}:
             return ""
         for item in self.props:
-            text = text + item +"="+self.props[item]+ " "
+            text = text + item +"="+self.props[item]
         return text
     def __repr__(self):
         text = (f"HTMLNODE: {self.tag}, {self.value}, {self.children}, {self.props}")
         return text
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag,value,props={}):
+        super(LeafNode,self).__init__()
+        self.tag = tag
+        self.value = value
+        self.props = props
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return str(self.value)
+        prop = self.props_to_html()
+
+        return f"<{self.tag}{prop}>{self.value}</{self.tag}>"
