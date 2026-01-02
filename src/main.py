b9fd622507608a2c8efc6cebbcfcab94a0d127de
heagge <henning.agge@gmail.com>
2026-01-02 11:38:09 +0100

    coomit2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from webside import copy_files_recursive, generate_page,generate_pages_recursive
 import sys
 import shutil
 def main():
-    shutil.rmtree("public")
+
     base = sys.argv[0]
     print("hello world")
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
