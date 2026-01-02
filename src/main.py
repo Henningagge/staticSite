359b77d1c83ca91a93903cb96f2bd0c3cdb3524c
heagge <henning.agge@gmail.com>
2026-01-02 10:52:19 +0100

    empty and delete public each main call

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from textnode import TextNode,TextType
 from webside import copy_files_recursive, generate_page
+import shutil
 def main():
+    shutil.rmtree("public")
     print("hello world")
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node.__repr__())
