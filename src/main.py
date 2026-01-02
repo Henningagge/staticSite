305d6c4bdc518c43c58f498bf9329299265eea7a
heagge <henning.agge@gmail.com>
2026-01-02 11:25:52 +0100

    update main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from textnode import TextNode,TextType
 from webside import copy_files_recursive, generate_page,generate_pages_recursive
+import sys
 import shutil
 def main():
     shutil.rmtree("public")
+    base = sys.argv[0]
     print("hello world")
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node.__repr__())
     copy_files_recursive("./static","./public")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", base)
 if __name__ == "__main__":
     main() 
